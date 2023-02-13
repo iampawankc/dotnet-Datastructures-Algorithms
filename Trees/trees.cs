3082cff8cb3e4199aeecfe567a983aabaacf911b
pawankc7 <pawan_21@live.com>
2023-02-13 10:08:29 +0530

    commit sunday

--- Trees/trees.cs
+++ Trees/trees.cs
@@ -12,26 +12,83 @@ namespace Trees
             //Traversal(Preorder,Inorder, Postorder, Levelorder)
             //Binary tree has atmost 2 children
 
+            TreeNode lnode = new TreeNode{
+                val=9, left=new TreeNode{val=1,right=null,left=null }, right= new TreeNode{val=2,right=null,left=null }
+            };
+            TreeNode rnode = new TreeNode{
+                val=20, left=new TreeNode{val=15,right=null,left=null }, right= new TreeNode{val=7,right=null,left=null }
+            };
+            TreeNode tree = new TreeNode{
+                val=3, left=lnode, right= rnode
+            };
 
-
+            //ulerTraversal(tree);
+            preorderTraversal(tree);
+            
         }
 
         public class TreeNode{
-            public int data;
+            public int val;
             public TreeNode left;
             public TreeNode right;
+            public  TreeNode(){}
 
-            TreeNode(int data){
-                this.data = data;
+            public TreeNode(int data){
+                this.val = data;
                 this.left=null;
                 this.right=null;
             }
 
-            TreeNode(int data, TreeNode left, TreeNode right){
-                this.data = data;
+            public TreeNode(int data, TreeNode left, TreeNode right){
+                this.val = data;
                 this.left = left;
                 this.right =right;
             }
         }
+
+        public static void preorderTraversal(TreeNode root){
+            if(root is null) {
+                return;
+            }
+
+
+            Console.WriteLine("Visiting "+root.val+" in preorder");
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+
+        public static void inorderTraversal(TreeNode root){
+            if(root is null) {
+                return;
+            }
+
+
+            inorderTraversal(root.left);
+            Console.WriteLine("Visiting "+root.val+" in inorder");
+            inorderTraversal(root.right);
+        }
+
+        public static void postorderTraversal(TreeNode root){
+            if(root is null) {
+                return;
+            }
+
+
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            Console.WriteLine("Visiting "+root.val+" in postorder");
+        }
+
+        public static void ulerTraversal(TreeNode root){
+            if(root is null) {
+                return;
+            }
+
+            Console.WriteLine("Visiting "+root.val+" first time");
+            ulerTraversal(root.left);
+            Console.WriteLine("Visiting "+root.val+" second time");
+            ulerTraversal(root.right);
+            Console.WriteLine("Visiting "+root.val+" third time");
+        }        
     }
-}   
+}
